005a2136c5f33f252f1f88222ec870374ec1c80b
que2017 <1563645183@qq.com>
2020-04-06 08:33:01 +0800

    与前端完成LoginServlet联调

--- src/com/duiyi/web/LoginServlet.java
+++ src/com/duiyi/web/LoginServlet.java
@@ -1,22 +1,19 @@
 package com.duiyi.web;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RegistServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println(request.getParameter("username"));
-		
-		response.getWriter().write("haha");
+
+		response.getWriter().write("wowo");
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
